03f8dd3174f55bd4a1f6bf0fcd93e268c841cc58
Mohamed Bouhadi <bouhadimed666@gmail.com>
2026-09-17 15:24:54 +0100

    start the menu

---
+++ src/index.js
@@ -0,0 +1,63 @@
+import { apprenants } from "./data.js";
+import {
+    afficherApprenant,
+    normaliserNom,
+    validerResultat,
+    ajouterApprenant,
+    enregistrerResultat,
+    rechercherApprenant,
+    calculerProgression,
+    filtrerParNiveau,
+    trierParProgression,
+    afficherTableauDeBord
+} from "./functions.js";
+
+import promptSync from "prompt-sync";
+
+const prompt = promptSync();
+
+let choice
+
+do {
+    console.log("SAS PROGRESS CONSOLE")
+    console.log("1. Afficher le tableau de bord")
+    console.log("2. Afficher la liste des apprenants")
+    console.log("3. Ajouter un apprenant")
+    console.log("4. Consulter un apprenant par identifiant")
+    console.log("5. Ajouter ou modifier le résultat d'une journée")
+    console.log("6. Rechercher un apprenant par nom")
+    console.log("7. Filtrer les apprenants par niveau")
+    console.log("8. Trier les apprenants par progression décroissante")
+    console.log("9. Trier les apprenants par ordre alphabétique")
+    console.log("0. Quitter")
+    choice = Number(prompt("What is your choice? "))
+
+    switch (choice) {
+        case 1: 
+            calculerProgression()
+            break
+        case 2:
+            afficherApprenant()
+            break
+        case 3:
+            let nom = prompt("Student's name: ")
+            nom = normaliserNom(nom)
+            ajouterApprenant(nom, ville)
+            break
+        // case 4:
+        //     break
+        // case 5:
+        //     break
+        // case 6:
+        //     break
+        // case 7:
+        //     break
+        // case 8:
+        //     break
+        // case 9:
+        //     break
+        // case 0:
+        //     break
+    }
+}while(choice != 0)
+
