038c72cec1d87bb44abf7116e87921b3299175e6
Mohamed Bouhadi <bouhadimed666@gmail.com>
2026-09-18 15:30:39 +0100

    set up my switch inside the menu

--- src/index.js
+++ src/index.js
@@ -11,7 +11,8 @@ import {
     filtrerParNiveau,
     trierParProgression,
     afficherTableauDeBord,
-    filtrerParAlphabet
+    filtrerParAlphabet,
+    displayAllLearners
 } from "./functions.js"
 
 import promptSync from "prompt-sync";
@@ -35,6 +36,83 @@ do {
 
     choice = Number(prompt("What is your choice? "));
 
-    
+    switch (choice) {
+
+        case 1:
+            afficherTableauDeBord();
+            break;
+
+        case 2:
+            displayAllLearners(apprenants);
+            break;
+
+        case 3: {
+            let nom = prompt("Student's name: ");
+            let ville = prompt("Student's city: ");
+
+            console.log(ajouterApprenant(nom, ville));
+            break;
+        }
+
+        case 4: {
+            let id = Number(prompt("Student's ID: "));
+
+            console.log(rechercherApprenant(id));
+            break;
+        }
+
+        case 5: {
+            let id = Number(prompt("Student's ID: "));
+            let jour = Number(prompt("Day (1-7): "));
+            let exercicesTermines = Number(prompt("Completed exercises: "));
+            let totalExercices = Number(prompt("Total exercises: "));
+            let challengeTermine = prompt("Challenge completed? (yes/no): ");
+
+            challengeTermine = challengeTermine.toLowerCase() === "yes";
+
+            console.log(
+                enregistrerResultat(
+                    id,
+                    jour,
+                    exercicesTermines,
+                    totalExercices,
+                    challengeTermine
+                )
+            )
+
+            break
+        }
+
+        case 6: {
+            let nom = prompt("Student's name: ");
+
+            console.log(rechercherApprenant(undefined, nom));
+            break;
+        }
+
+        case 7: {
+            let niveau = prompt(
+                "Level (Solide / En progression / À renforcer): "
+            )
+
+            console.log(filtrerParNiveau(niveau));
+            break
+        }
+
+        case 8:
+            console.log(trierParProgression());
+            break;
+
+        case 9:
+            console.log(filtrerParAlphabet(apprenants));
+            break;
+
+        case 0:
+            console.log("Goodbye!");
+            break
+
+        default:
+            console.log("Invalid choice. Please choose a number between 0 and 9.");
+    }
 
 } while (choice !== 0)
