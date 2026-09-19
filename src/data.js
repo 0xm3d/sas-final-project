137d0f32c99270325da7b56a4c2d1f4f5779642f
Mohamed Bouhadi <bouhadimed666@gmail.com>
2026-09-19 01:05:04 +0100

    add more data

--- src/data.js
+++ src/data.js
@@ -1,17 +1,11 @@
- export const apprenants = [
+export const apprenants = [
     {
         id: 1,
         nomComplet: "Sara Dev",
         ville: "Nador",
         resultats: [
-            {
-                jour: 1, exercicesTermines: 18,
-                totalExercices: 20, challengeTermine: true
-            },
-            {
-                jour: 2, exercicesTermines: 14,
-                totalExercices: 20, challengeTermine: false
-            }
+            { jour: 1, exercicesTermines: 18, totalExercices: 20, challengeTermine: true },
+            { jour: 2, exercicesTermines: 14, totalExercices: 20, challengeTermine: false }
         ]
     },
     {
@@ -19,11 +13,78 @@
         nomComplet: "Yassine Code",
         ville: "Oujda",
         resultats: [
-            {
-                jour: 1, exercicesTermines: 12,
-                totalExercices: 20, challengeTermine: false
-            }
+            { jour: 1, exercicesTermines: 12, totalExercices: 20, challengeTermine: false }
+        ]
+    },
+    {
+        id: 3,
+        nomComplet: "Imane Web",
+        ville: "Casablanca",
+        resultats: [
+            { jour: 1, exercicesTermines: 20, totalExercices: 20, challengeTermine: true },
+            { jour: 2, exercicesTermines: 18, totalExercices: 20, challengeTermine: true },
+            { jour: 3, exercicesTermines: 19, totalExercices: 20, challengeTermine: true }
+        ]
+    },
+    {
+        id: 4,
+        nomComplet: "Lina Code",
+        ville: "Rabat",
+        resultats: [
+            { jour: 1, exercicesTermines: 10, totalExercices: 20, challengeTermine: false },
+            { jour: 2, exercicesTermines: 8, totalExercices: 20, challengeTermine: false }
+        ]
+    },
+    {
+        id: 5,
+        nomComplet: "Omar Dev",
+        ville: "Marrakech",
+        resultats: [
+            { jour: 1, exercicesTermines: 15, totalExercices: 20, challengeTermine: true },
+            { jour: 2, exercicesTermines: 16, totalExercices: 20, challengeTermine: true }
+        ]
+    },
+    {
+        id: 6,
+        nomComplet: "Yassmine JS",
+        ville: "Fes",
+        resultats: [
+            { jour: 1, exercicesTermines: 5, totalExercices: 20, challengeTermine: false },
+            { jour: 2, exercicesTermines: 6, totalExercices: 20, challengeTermine: false }
+        ]
+    },
+    {
+        id: 7,
+        nomComplet: "Karim Node",
+        ville: "Tanger",
+        resultats: [
+            { jour: 1, exercicesTermines: 17, totalExercices: 20, challengeTermine: true },
+            { jour: 2, exercicesTermines: 19, totalExercices: 20, challengeTermine: true },
+            { jour: 3, exercicesTermines: 18, totalExercices: 20, challengeTermine: true },
+            { jour: 4, exercicesTermines: 20, totalExercices: 20, challengeTermine: true }
+        ]
+    },
+    {
+        id: 8,
+        nomComplet: "Nada Front",
+        ville: "Agadir",
+        resultats: []
+    },
+    {
+        id: 9,
+        nomComplet: "Hamza Back",
+        ville: "Meknes",
+        resultats: [
+            { jour: 1, exercicesTermines: 13, totalExercices: 20, challengeTermine: false },
+            { jour: 2, exercicesTermines: 15, totalExercices: 20, challengeTermine: true }
+        ]
+    },
+    {
+        id: 10,
+        nomComplet: "Salma Test",
+        ville: "Kenitra",
+        resultats: [
+            { jour: 1, exercicesTermines: 4, totalExercices: 20, challengeTermine: false }
         ]
     }
-]
-
+];
